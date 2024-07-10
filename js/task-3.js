410503d319e86a4cd2ae89fd24dfcb5628a51d61
Valeriia <malvinacat0404@gmail.com>
2024-07-10 17:35:26 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,7 @@ function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
+    
     const totalWidth = contentWidth + paddingWidth + borderWidth;
     return totalWidth;
 }
