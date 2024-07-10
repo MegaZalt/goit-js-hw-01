be2903132c9505f29d84435e03ce4b710fd5ca72
Valeriia <malvinacat0404@gmail.com>
2024-07-10 17:06:10 +0200

    5

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,8 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const
-    return `Shipping to <country> will cost <totalPrice> credits`;
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
+
+console.log(getShippingMessage("Australia", 120, 50)); // 170 
+console.log(getShippingMessage("Germany", 80, 20)); // 100 
+console.log(getShippingMessage("Sweden", 100, 20)); // 120 
